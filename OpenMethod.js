663ad15ad126253768a0ed185119b53475391ed4
Muh. Rifqi Muafa <muhammadrifqi20001@gmail.com>
2023-04-06 21:16:07 +0800

    menjadikan semua pilihan dalam fungsi

--- OpenMethod.js
+++ OpenMethod.js
@@ -1,5 +1,7 @@
 const prompt = require('prompt-sync')(); // we have to install module prompt-sync from npm (npm install prompt-sync)
 
+let tabel = [];
+
 function signToUnsign (value){
     if(value > 0){
         return value;
@@ -8,6 +10,88 @@ function signToUnsign (value){
     }
 }
 
+function bisectionMethod (nowxl, nowxu){
+    let xrpre = 0;
+    let ea = 100;
+
+    for(i = 1; i<=20; i++){
+        if(signToUnsign(ea) < 2){
+            break;
+        }
+    
+        xu = parseFloat(nowxu);
+        xl = parseFloat(nowxl);
+        fxl = (5-(5*(xl))-(Math.E**(xl/2)));
+        fxu = (5-(5*(xu))-(Math.E**(xu/2)));
+        
+        if(fxl*fxu > 0){
+            console.log('\nTidak memenuhi syarat (fxl*fxu > 0)');
+            break;
+        }
+
+        xr = (xl + xu)/2
+        fxr = (5-(5*(xr))-(Math.E**(xr/2)));
+        ea = ((xr-xrpre)/xr)*100;
+
+        tabel.push({iteration : i, xl : xl.toFixed(3), fxl : fxl.toFixed(3), xu: xu.toFixed(3), fxu: fxu.toFixed(3), xr: xr.toFixed(3), fxr: fxr.toFixed(3), ea: signToUnsign(ea).toFixed(3)});
+    
+        if((fxl > 0 && fxr > 0) || (fxl < 0 && fxr < 0)){
+            nowxu = xu;
+            nowxl = xr;
+            xrpre = xr;
+        }else if ((fxl > 0 && fxr < 0) || (fxl < 0 && fxr > 0)){
+            nowxu = xr;
+            nowxl = xl;
+            xrpre = xr;
+        }
+    }
+
+    return tabel;
+}
+
+function newtonRaphsonMethod (xo){
+    let ea = 100;
+
+    for(i = 1; i <= 20; i++){
+        if(signToUnsign(ea) < 2){
+            break;
+        }
+
+        fx = (5-(5*(xo))-(Math.E**(xo/2)));
+        fxa = ((-5) - ((Math.E**(xo/2))/2));
+
+        xi = xo-(fx/fxa); 
+        ea = ((xi - xo)/xi)*100;
+
+        tabel.push({i : i, xo : xo.toFixed(3) , 'f(xo)' : fx.toFixed(3) , 'f`(xo)' : fxa.toFixed(3) , xi : (xi.toFixed(3)), 'ea (%)' : signToUnsign(ea).toFixed(2)});
+        xo = xi;
+    }
+
+    return tabel;
+}
+
+function secantMethod (x_1, xo){
+    let ea = 100;
+
+    for(i = 1; i < 20; i++){
+        if(signToUnsign(ea) < 2){
+            break;
+        }
+
+        fxm = (5-(5*(x_1))-(Math.E**(x_1/2)));
+        fxo = (5-(5*(xo))-(Math.E**(xo/2)));
+        xi = xo-((fxo*(x_1-xo)))/(fxm-fxo);
+        ea = ((xi - xo)/xi)*100;
+
+        tabel.push({i : i, 'x-1' : x_1.toFixed(3), 'f(x-1)' : fxm.toFixed(3) , 'xo' : xo.toFixed(3) , 'f(x0)' : fxo.toFixed(3), 'xi' : (xi.toFixed(3)), 'ea (%)' : signToUnsign(ea).toFixed(2)});
+        x_1 = xo;
+        xo = xi;
+    }
+
+    return tabel;
+}
+
+let hasil;
 let kondisi = true;
 let j = 1;
 
@@ -27,44 +111,10 @@ while(kondisi){
         let nowxl = prompt("Masukkan initial guess xl: ");
         let nowxu = prompt("Masukkan initial guess xu: ");
 
-        let xrpre = 0;
-        let ea = 100;
-        let tabel = [];
+        hasil = bisectionMethod(nowxl, nowxu);
 
-        for(i = 1; i<=20; i++){
-            if(signToUnsign(ea) < 2){
-                break;
-            }
-        
-            xu = parseFloat(nowxu);
-            xl = parseFloat(nowxl);
-            fxl = (5-(5*(xl))-(Math.E**(xl/2)));
-            fxu = (5-(5*(xu))-(Math.E**(xu/2)));
-            
-            if(fxl*fxu > 0){
-                console.log('\nTidak memenuhi syarat (fxl*fxu > 0)');
-                break;
-            }
-
-            xr = (xl + xu)/2
-            fxr = (5-(5*(xr))-(Math.E**(xr/2)));
-            ea = ((xr-xrpre)/xr)*100;
-
-            tabel.push({iteration : i, xl : xl.toFixed(3), fxl : fxl.toFixed(3), xu: xu.toFixed(3), fxu: fxu.toFixed(3), xr: xr.toFixed(3), fxr: fxr.toFixed(3), ea: signToUnsign(ea).toFixed(3)});
-        
-            if((fxl > 0 && fxr > 0) || (fxl < 0 && fxr < 0)){
-                nowxu = xu;
-                nowxl = xr;
-                xrpre = xr;
-            }else if ((fxl > 0 && fxr < 0) || (fxl < 0 && fxr > 0)){
-                nowxu = xr;
-                nowxl = xl;
-                xrpre = xr;
-            }
-        }
-        console.table(tabel);
+        console.table(hasil);
         console.log('\n-----------------------------------------------------------------------------------------------------------\n');
-        delete tabel;
 
     }else if(parseInt(pilihan)===2){
         console.log('------------------------');
@@ -73,26 +123,11 @@ while(kondisi){
 
         let initialGuess = prompt("Masukkan initial guess xi: ");
         let xo = parseFloat(initialGuess);
-        let ea = 100;
-        let tabel = [];
+        
+        hasil = newtonRaphsonMethod(xo);
 
-        for(i = 1; i <= 20; i++){
-            if(signToUnsign(ea) < 2){
-                break;
-            }
-
-            fx = (5-(5*(xo))-(Math.E**(xo/2)));
-            fxa = ((-5) - ((Math.E**(xo/2))/2));
-
-            xi = xo-(fx/fxa); 
-            ea = ((xi - xo)/xi)*100;
-
-            tabel.push({i : i, xo : xo.toFixed(3) , 'f(xo)' : fx.toFixed(3) , 'f`(xo)' : fxa.toFixed(3) , xi : (xi.toFixed(3)), 'ea (%)' : signToUnsign(ea).toFixed(2)});
-            xo = xi;
-        }
-        console.table(tabel);
+        console.table(hasil);
         console.log('\n-----------------------------------------------------------------------------------------------------------\n');
-        delete tabel;
 
     }else if(parseInt(pilihan)===3){
         console.log('----------------');
@@ -104,27 +139,11 @@ while(kondisi){
 
         let x_1 = parseFloat(value1);
         let xo = parseFloat(value2);
-        let ea = 100;
-        let tabel = [];
 
-        for(i = 1; i < 20; i++){
-            if(signToUnsign(ea) < 2){
-                break;
-            }
+        hasil = secantMethod(x_1, xo);
 
-            fxm = (5-(5*(x_1))-(Math.E**(x_1/2)));
-            fxo = (5-(5*(xo))-(Math.E**(xo/2)));
-            xi = xo-((fxo*(x_1-xo)))/(fxm-fxo);
-            ea = ((xi - xo)/xi)*100;
-
-            tabel.push({i : i, 'x-1' : x_1.toFixed(3), 'f(x-1)' : fxm.toFixed(3) , 'xo' : xo.toFixed(3) , 'f(x0)' : fxo.toFixed(3), 'xi' : (xi.toFixed(3)), 'ea (%)' : signToUnsign(ea).toFixed(2)});
-            x_1 = xo;
-            xo = xi;
-        }
-
-        console.table(tabel);
+        console.table(hasil);
         console.log('\n-----------------------------------------------------------------------------------------------------------\n');
-        delete tabel;
 
     }else if(parseInt(pilihan)===4){
         kondisi = false;
@@ -133,5 +152,6 @@ while(kondisi){
         console.log('Pilihan tidak tersedia!\n');
     }
 
+    tabel.splice(0, tabel.length);
     j++
 }
